4dd8ec1561ef31349160c7f938c6d2488ba666d2
Kaique Silva <kaeyosthaeron@gmail.com>
2020-05-07 19:37:45 -0300

    Check the continue flag

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const http = require('http');
 
 const args = process.argv.slice(2);
 
-function requestClient(url='') {
+function requestClient(url='', c=false) {
   const agent = new http.Agent({ keepAlive: true });
   const req = http
     .get(url, { agent }, res => {
       console.log(`Status: ${res.statusCode}`);
+      if (!c) process.exit(0);
     })
-    .on('error', console.log.bind(console));
+    .on('error', (err) => {
+      console.log(err);
+      if (!c) process.exit(1);
+    });
 
     return req;
 }
@@ -19,6 +23,6 @@ if (args.length === 1) {
   requestClient(args[0]);
 } else if (args.length === 2 && (args[1] === '-c' || args[1] === '--continue')) {
   setInterval(() => {
-    requestClient(args[0]);
+    requestClient(args[0], true);
   }, 1000);
 }
